42a4c29d24233656fb0a76bfa798753f4899d5a5
Muraveyy <antmuravey@gmail.com>
2016-02-23 18:43:32 +0300

    testing github

---
+++ Kursach_LAST_TEST/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Kursach_LAST_TEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label3 = new System.Windows.Forms.Label();
+            this.free = new System.Windows.Forms.Button();
+            this.BOMB = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.refresh = new System.Windows.Forms.Button();
+            this.FIELD = new System.Windows.Forms.TextBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "процент мин";
+            // 
+            // free
+            // 
+            this.free.Enabled = false;
+            this.free.Location = new System.Drawing.Point(9, 79);
+            this.free.Name = "free";
+            this.free.Size = new System.Drawing.Size(40, 40);
+            this.free.TabIndex = 45;
+            this.free.Text = "FREE";
+            this.free.UseVisualStyleBackColor = true;
+            this.free.Visible = false;
+            // 
+            // BOMB
+            // 
+            this.BOMB.Enabled = false;
+            this.BOMB.Location = new System.Drawing.Point(9, 33);
+            this.BOMB.Name = "BOMB";
+            this.BOMB.Size = new System.Drawing.Size(40, 40);
+            this.BOMB.TabIndex = 44;
+            this.BOMB.Text = "BOMB";
+            this.BOMB.UseVisualStyleBackColor = true;
+            this.BOMB.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("NI7SEG", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(55, 55);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(377, 20);
+            this.textBox1.TabIndex = 43;
+            this.textBox1.Text = "RESIZE WINDOW AS YOU WANT AND PRESS PLAY ";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Mistral", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(58, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 38);
+            this.button1.TabIndex = 42;
+            this.button1.Text = "START";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(188, 26);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(104, 23);
+            this.refresh.TabIndex = 54;
+            this.refresh.Text = "Refresh FIELD";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Visible = false;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // FIELD
+            // 
+            this.FIELD.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FIELD.Enabled = false;
+            this.FIELD.Font = new System.Drawing.Font("NI7SEG", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FIELD.ForeColor = System.Drawing.Color.Maroon;
+            this.FIELD.Location = new System.Drawing.Point(0, 79);
+            this.FIELD.Multiline = true;
+            this.FIELD.Name = "FIELD";
+            this.FIELD.ReadOnly = true;
+            this.FIELD.Size = new System.Drawing.Size(529, 508);
+            this.FIELD.TabIndex = 41;
+            this.FIELD.Text = "Wait a little bit, please.\r\n";
+            this.FIELD.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.FIELD.Visible = false;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(12, 7);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown3.TabIndex = 52;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(422, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 51;
+            this.label2.Text = "столбцы";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(430, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 50;
+            this.label1.Text = "строки";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown2.Location = new System.Drawing.Point(478, 29);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDown2.TabIndex = 49;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(478, 3);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDown1.TabIndex = 48;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Freestyle Script", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.textBox2.Location = new System.Drawing.Point(132, 3);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(210, 24);
+            this.textBox2.TabIndex = 47;
+            this.textBox2.Text = "limit of buttons is 30x30";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(9, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 40);
+            this.button2.TabIndex = 46;
+            this.button2.Text = "close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(528, 591);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.free);
+            this.Controls.Add(this.BOMB);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.FIELD);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button free;
+        private System.Windows.Forms.Button BOMB;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.TextBox FIELD;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Kursach_LAST_TEST/Form1.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Kursach_LAST_TEST
+{
+    public partial class Form1 : Form
+    {
+
+
+        int p, f;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "Press PLAY to play!!!!! ";
+
+
+            Timer timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000;
+            timer1.Start();
+
+
+        }
+        //Button[,] bt2 = new Button[31, 31];
+
+
+        int[,] sss = new int[33, 33];
+
+
+        Button[,] bt = new Button[32, 32];
+        TextBox[,] tb = new TextBox[32, 32];
+
+        //int[,] set2 = new int[33,33];
+
+        private void button1_Click(object sender, EventArgs e)//создание минного поля
+        {
+            int wid = (int)numericUpDown1.Value;
+            int hei = (int)numericUpDown2.Value;
+
+            int x = -FIELD.Width / wid, y = 0;
+            button1.Dispose();
+
+            textBox2.Text = "Enjoy Your Game";
+
+
+            //создаем массив содержащий местоположения мин
+            Random rnd = new Random();
+
+            int[,] set = new int[wid + 2, hei + 2];
+            int rand;
+
+            for (int i = 1; i < wid + 1; i++)
+            {
+                for (int j = 1; j < hei + 1; j++)
+                {
+                    rand = rnd.Next(0, 100);
+                    if (rand < (int)numericUpDown3.Value)
+                        set[i, j] = 9;
+                    textBox1.Text = textBox1.Text + set[i, j].ToString();
+                }
+            }
+
+            //изменяем массив для описания мест бомб
+            for (int i = 1; i < wid + 1; i++)//Номер столбца
+            {
+                for (int j = 1; j < hei + 1; j++)//номер строки
+                {
+
+
+                    if ((i != 0 || j != 0) && (set[i, j] != 9) && (set[i - 1, j - 1] == 9))//лево верх
+                        set[i, j]++;
+                    if ((i != 0) && (set[i, j] != 9) && (set[i - 1, j] == 9))//лево
+                        set[i, j]++;
+                    if ((i != 0 || j != hei) && (set[i, j] != 9) && (set[i - 1, j + 1] == 9))//лево низ
+                        set[i, j]++;
+
+                    if ((j != 0) && (set[i, j] != 9) && (set[i, j - 1] == 9))//верх
+                        set[i, j]++;
+                    if ((j != wid) && (set[i, j] != 9) && (set[i, j + 1] == 9))//низ
+                        set[i, j]++;
+
+                    if ((i != wid || j != 0) && (set[i, j] != 9) && (set[i + 1, j - 1] == 9))//право верх
+                        set[i, j]++;
+                    if ((i != wid) && (set[i, j] != 9) && (set[i + 1, j] == 9))//право
+                        set[i, j]++;
+                    if ((i != wid || j != wid) && (set[i, j] != 9) && (set[i + 1, j + 1] == 9))//право низ
+                        set[i, j]++;
+                }
+            }
+
+
+            int k = 0;
+
+            for (int i = 1; i < wid + 1; i++)
+            {
+                for (int j = 1; j < hei + 1; j++)
+                {
+                    //Создаем новую кнопку
+                    Button temp = new Button();
+                    TextBox te = new TextBox();
+
+
+                    if (set[i, j] != 0)
+                        te.Text = set[i, j].ToString();// для текста
+                    if (set[i, j] == 9)
+                    {
+                        te.BackColor = Color.Black;
+                        te.ForeColor = Color.OrangeRed;
+                    }
+
+                    temp.Width = FIELD.Width / wid;
+                    temp.Height = FIELD.Height / (hei + 1);
+                    //  temp.FlatStyle = FlatStyle.Flat;//изменение режима отображения на плоский
+
+                    te.Multiline = true;
+
+                    te.Width = FIELD.Width / wid;
+                    te.Height = FIELD.Height / (hei + 1);
+
+                    te.Enabled = false;
+                    //Размещаем ее правее(на x) (и ниже на y) кнопки, на которую мы нажали
+                    //te.Location = new Point(b.Location.X + b.Width + x, b.Location.Y + y);
+                    te.Location = new Point(FIELD.Location.X + FIELD.Width / wid + x, FIELD.Location.Y + FIELD.Height / (hei + 1) + y);
+                    temp.Location = new Point(FIELD.Location.X + FIELD.Width / wid + x, FIELD.Location.Y + FIELD.Height / (hei + 1) + y);
+
+                    //Добавляем событие нажатия на новую кнопку 
+                    bt[i, j] = temp;
+                    tb[i, j] = te;
+                    p = i; f = j;
+
+                    if (set[i, j] == 0)
+                    {
+                        bt[i, j].Click += new EventHandler(free_Click);
+                        bt[i, j].MouseUp += new MouseEventHandler(free_MouseUp);
+                    }
+
+                    if ((set[i, j] != 0) && (set[i, j] != 9))
+                    {
+                        bt[i, j].Click += new EventHandler(button2_Click);
+                        bt[i, j].MouseUp += new MouseEventHandler(free_MouseUp);
+                    }
+
+                    if (set[i, j] == 9)
+                    {
+
+                        bt[i, j].Click += new EventHandler(BOMB_NOT_Click);
+                        bt[i, j].MouseUp += new MouseEventHandler(free_MouseUp);
+
+                    }
+
+                    //Добавляем элемент на форму
+
+                    this.Controls.Add(temp);
+                    this.Controls.Add(te);
+                    k++;
+                    y += FIELD.Height / (hei + 1);
+                }
+
+                x += FIELD.Width / wid;
+                y = 0;
+
+
+            }
+
+            //  button1.Dispose();
+
+
+            for (int i = 1; i < wid + 1; i++)
+                for (int j = 1; j < hei + 1; j++)
+                    sss[i, j] = set[i, j];
+
+            // numericUpDown1.Dispose();
+            numericUpDown1.Hide();
+            // numericUpDown2.Dispose();
+            numericUpDown2.Hide();
+
+            //numericUpDown3.Dispose();
+            numericUpDown3.Hide();
+
+            label1.Dispose();
+            label2.Dispose();
+            label3.Dispose();
+
+
+            refresh.Visible = true;
+        }
+
+        private void Mouse_RightClick(object sender, EventArgs e)
+        {
+            Button temp = (Button)sender;
+            temp.ForeColor = Color.Violet;
+            temp.FlatStyle = FlatStyle.Flat;
+            temp.BackColor = Color.Gold;
+        }
+
+        private void BOMBB(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BOMB_NOT_Click(object sender, EventArgs e)
+        {
+            Button temp = (Button)sender;
+            if (temp.BackColor != Color.Gold)
+            {
+                temp.ForeColor = Color.White;
+                temp.FlatStyle = FlatStyle.Flat;
+                temp.BackColor = Color.Empty;
+
+                for (int i = 1; i < 31; i++)
+                    for (int j = 1; j < 31; j++)
+                        if (sss[i, j] == 9)
+                        {
+
+                            bt[i, j].Visible = false;
+                            bt[i, j].Enabled = false;
+
+                        }
+
+
+
+
+
+                textBox2.Text = "YOU GOT KILLED.";
+                Button b = (Button)sender;
+                //Создаем новую кнопку
+                Button temp2 = new Button();
+                temp2.Width = 50;
+                temp2.Height = 25;
+                temp2.Text = "Exit";
+                temp2.ForeColor = Color.Gold;
+                temp2.FlatStyle = FlatStyle.Flat;
+                temp2.BackColor = Color.Black;
+                temp2.Location = new Point(0, 0);
+
+
+                this.Controls.Add(temp2);
+                temp2.Click += new EventHandler(BOMBB);
+
+            }
+            else if (temp.BackColor == Color.Gold)
+            {
+                temp.ForeColor = Color.Black;
+                temp.FlatStyle = FlatStyle.Standard;
+                temp.BackColor = Color.Empty;
+            }
+        }
+
+        private void free_Click(object sender, EventArgs e)
+        {
+            Button temp = (Button)sender;
+            if (temp.BackColor != Color.Gold)
+            {
+                for (int i = 1; i < 31; i++)
+                {
+                    for (int j = 1; j < 31; j++)
+                    {
+                        if (bt[i, j] == temp)
+                        {
+                            temp.Visible = false;
+                            temp.Enabled = false;
+                            // temp.Dispose();
+
+                            if (bt[i, j + 1] != null && bt[i, j + 1].BackColor != Color.Gold)
+                                bt[i, j + 1].PerformClick();
+
+                            if (bt[i + 1, j] != null && bt[i + 1, j].BackColor != Color.Gold)
+                                bt[i + 1, j].PerformClick();
+
+                            if (bt[i - 1, j] != null && bt[i - 1, j].BackColor != Color.Gold)
+                                bt[i - 1, j].PerformClick();
+
+                            if (bt[i, j - 1] != null && bt[i, j - 1].BackColor != Color.Gold)
+                                bt[i, j - 1].PerformClick();
+
+
+                            if (bt[i + 1, j + 1] != null && bt[i + 1, j + 1].BackColor != Color.Gold)
+                                bt[i + 1, j + 1].PerformClick();
+
+                            if (bt[i + 1, j - 1] != null && bt[i + 1, j - 1].BackColor != Color.Gold)
+                                bt[i + 1, j - 1].PerformClick();
+
+                            if (bt[i - 1, j + 1] != null && bt[i - 1, j + 1].BackColor != Color.Gold)
+                                bt[i - 1, j + 1].PerformClick();
+
+                            if (bt[i - 1, j - 1] != null && bt[i - 1, j - 1].BackColor != Color.Gold)
+                                bt[i - 1, j - 1].PerformClick();
+                        }
+                    }
+                }
+            }
+            else if (temp.BackColor == Color.Gold)
+            {
+                temp.ForeColor = Color.Black;
+                temp.FlatStyle = FlatStyle.Standard;
+                temp.BackColor = Color.Empty;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime ThToday = DateTime.Now;
+            string ThData = ThToday.ToString();
+            this.textBox1.Text = ThData;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button temp = (Button)sender;
+            if (temp.BackColor != Color.Gold)
+            {
+                // temp.Dispose();
+                temp.Visible = false;
+                temp.Enabled = false;
+            }
+            else if (temp.BackColor == Color.Gold)
+            {
+                temp.ForeColor = Color.Black;
+                temp.FlatStyle = FlatStyle.Standard;
+                temp.BackColor = Color.Empty;
+            }
+        }
+
+        private void free_MouseUp(object sender, MouseEventArgs e)
+        {
+            Button temp = (Button)sender;
+            if (e.Button == MouseButtons.Right)
+            {
+                temp.ForeColor = Color.Violet;
+                temp.FlatStyle = FlatStyle.Flat;
+                temp.BackColor = Color.Gold;
+            }
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            int wid = (int)numericUpDown1.Value;
+            int hei = (int)numericUpDown2.Value;
+
+            int x = -FIELD.Width / wid, y = 0;
+            textBox2.Text = "Wait a little bit, please.";
+            FIELD.Visible = true;
+
+
+            for (int i = 1; i < wid + 1; i++)
+            {
+                for (int j = 1; j < hei + 1; j++)
+                {
+
+
+                    //    if(bt[i,j].Enabled==false)
+                    //  bt[i, j].Visible = false;
+
+                    // tb[i, j].Visible = false;
+
+                    bt[i, j].Width = FIELD.Width / wid;
+                    bt[i, j].Height = FIELD.Height / (hei + 1);
+
+                    tb[i, j].Width = FIELD.Width / wid;
+                    tb[i, j].Height = FIELD.Height / (hei + 1);
+
+                    tb[i, j].Location = new Point(FIELD.Location.X + FIELD.Width / wid + x, FIELD.Location.Y + FIELD.Height / (hei + 1) + y);
+
+                    bt[i, j].Location = new Point(FIELD.Location.X + FIELD.Width / wid + x, FIELD.Location.Y + FIELD.Height / (hei + 1) + y);
+
+                    y += FIELD.Height / (hei + 1);
+
+
+                }
+
+                x += FIELD.Width / wid;
+                y = 0;
+
+
+            }
+
+
+            FIELD.Visible = false;
+
+            textBox2.Text = "Enjoy Your Game";
+
+
+
+
+
+        }
+
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            textBox2.Text = "Changed";
+        }
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
